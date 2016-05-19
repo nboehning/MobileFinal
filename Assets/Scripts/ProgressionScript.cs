e5691dac3be16cc154b2870fd0c23788ece27216
svc-student <svc-student@student.directory.jccc>
2016-05-19 14:48:19 -0500

    Added in victors things
    Added in Victors things, and added final spells

--- Assets/Scripts/ProgressionScript.cs
+++ Assets/Scripts/ProgressionScript.cs
@@ -5,18 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class ProgressionScript : MonoBehaviour {
 
+    //elements designed for the game
+    [Tooltip("Beginning time for the in lights game per level in Infinite mode.")]
     public float lightsBeginningTime = 40f;
+    [Tooltip("Beginning time for the in spells game per level in Infinite mode.")]
     public float spellsBeginningTime = 20f;
+    [Tooltip("Percentage of time removed from current counter for next level. 1st tier.")]
     public float percentageReduced1 = 0.059f;
+    [Tooltip("The level at which the percentage subtracted from the time counter is changed from percentageReduced1 to percentageReduced2")]
     public int levelThreshold2 = 10;
+    [Tooltip("Percentage of time removed from current counter for next level. 2nd tier.")]
     public float percentageReduced2 = 0.035f;
+    [Tooltip("The level at which the percentage subtracted from the time counter is changed from percentageReduced2 to percentageReduced3")]
     public int levelThreshold3 = 25;
+    [Tooltip("Percentage of time removed from current counter for next level. 3rd tier.")]
     public float percentageReduced3 = 0.028f;
+    [Tooltip("percentage of time in game from level/infinite mode that the player is notified they are reaching the end.")]
     public float percentForWrongPrompt = 0.318f;
-    public float modTime;
+    //placeholder. Keeps track of start time for current level (infinite) or total time (timed)
+    float modTime;
+    //current time left in level/game
     float currentTime;
-
+    //keeps track of the levels completed. Will not count current level passed. 
     int currentLevel;
+
     bool checkedMax;
 
     public Text[] timeLeftTitles;
@@ -78,16 +90,19 @@ public class ProgressionScript : MonoBehaviour {
     {
 
         currentLevel += 1;
-        float mod;
-        if (currentLevel <= levelThreshold2)
-            mod = percentageReduced1;
-        else if (currentLevel <= levelThreshold3)
-            mod = percentageReduced2;
-        else
-            mod = percentageReduced3;
-        modTime -= modTime * mod;
-        currentTime = modTime;
-        SetTheMarquee();
+        if (PrefStatsScript.gameType == PrefStatsScript.GameType.INFINITE)
+        {
+            float mod;
+            if (currentLevel <= levelThreshold2)
+                mod = percentageReduced1;
+            else if (currentLevel <= levelThreshold3)
+                mod = percentageReduced2;
+            else
+                mod = percentageReduced3;
+            modTime -= modTime * mod;
+            currentTime = modTime;
+            SetTheMarquee();
+        }
     }
 
     void SetTheMarquee()
